ce1d288fc2b64b9024335394b6cce392522be365
shanjayvarun <82082315+shanjayvarun@users.noreply.github.com>
2021-08-28 22:32:18 +0530

    Add files via upload

---
+++ README.markdown
@@ -0,0 +1,6 @@
+# Pagination With Shan
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/VERSHAN-X/pen/LYLpLQy](https://codepen.io/VERSHAN-X/pen/LYLpLQy).
+
+Can anyone Teach me how to Incorporate JS here?
+
---
+++ script.js
@@ -0,0 +1,13 @@
+function ABOUT() {
+  document.getElementById("About").innerHTML =
+    "| I Currently hold a bachelor of Textile Technology from the Anna university. From 2018, I worked as a Marketing Merchandiser for Celebrity Fashions ltd - Indian Terrain, a leading Textile fashion brand of historical Madras for One and Half years. I Currently live in Chennai with my Lovable Sister and My Dog Called Lucky 🐶 |";
+}
+
+function ACHEIVEMENTS() {
+  document.getElementById("About").innerHTML =
+    "| Best Performer of the Season - Timberland SS’21 of VFC corporation | GATE 2018 - 2021 Qualified Candidate Securing an All India Rank of 333 | Holding a State 3rd and School Topper in HSE French with 196/200 in the year 2014 | Trophy Winner – Sis. Lydia’s Memorial Chennai District Interschool Badminton Trophy 2013 ";
+}
+function HOBBIES() {
+  document.getElementById("About").innerHTML =
+    "| I play Good Club Badminton and keeping up good Fitness by Working out in Gym | Love to travel | House Cleaning | Driving cars | Roaming around with Friends | Learning to Cook since 2018 😂 |";
+}
